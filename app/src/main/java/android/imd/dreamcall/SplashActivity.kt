f26c9880f47e53531a516030b20a1e2edcfb7f8e
Victor Simplicio <kuro_caelum@outlook.com>
2019-11-17 18:47:27 -0300

    Splash added

---
+++ app/src/main/java/android/imd/dreamcall/SplashActivity.kt
@@ -0,0 +1,20 @@
+package android.imd.dreamcall
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Thread{
+            SystemClock.sleep(3000)
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }.start()
+    }
+}
